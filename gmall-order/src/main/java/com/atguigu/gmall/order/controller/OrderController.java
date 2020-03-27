b60275d43682dc66be9ff3c87076a7bc5e7c6205
2411639387@qq.com <2411639387@qq.com>
2020-03-27 17:04:42 +0800

    master提交order模块

---
+++ gmall-order/src/main/java/com/atguigu/gmall/order/controller/OrderController.java
@@ -0,0 +1,12 @@
+package com.atguigu.gmall.order.controller;
+
+/**
+ * OrderController
+ *
+ * @Author: 傅逸桐
+ * @CreateTime: 2020-03-27
+ * @Description: 用来表示javaBean
+ */
+public class OrderController {
+
+}
