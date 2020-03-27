514295b0658b7b879e58c76d5713e946b6fdc345
2411639387@qq.com <2411639387@qq.com>
2020-03-27 17:06:38 +0800

    master提交pay模块

---
+++ gmall-pay/src/main/java/com/atguigu/gmall/pay/controller/PayController.java
@@ -0,0 +1,15 @@
+package com.atguigu.gmall.pay.controller;
+
+/**
+ * PayController
+ *
+ * @Author: 傅逸桐
+ * @CreateTime: 2020-03-27
+ * @Description: 用来表示javaBean
+ */
+public class PayController {
+
+    public void pay(){
+
+    }
+}
